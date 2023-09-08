b24aee04abafd8b0b906b97eade3b9d4632fccce
MeseryGamer <meserygamer@outlook.com>
2023-09-08 13:07:47 +0300

    Добавлена функция с решением квадратного уравнения

--- Prakt1/Source.c
+++ Prakt1/Source.c
@@ -1,35 +1,38 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <windows.h>
+#include <math.h>
 
 #pragma region FuncPrototypes
 
-void StartInfoToConsole();
-int TaskSelection();
+void startInfoToConsole();
+int taskSelection();
+double* solutionQuadraticEquation(double a, double b, double c);
 
 #pragma endregion
 
 int main()
 {
-	StartInfoToConsole();
-	TaskSelection();
+	startInfoToConsole(); //Настройка консоли и вывод стартовой информации
+	int userChoice = taskSelection(); //Выбор пользователем задачи 
+	solutionQuadraticEquation(1, 4, 3);
 	return 0;
 }
 
-void StartInfoToConsole()
+void startInfoToConsole()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	printf("Выберите задачу для решения:\n1) Квадратное уравнение\n2) Cистема двух линейных уравнений\n3) Перестановки (факториал)\n");
 }
 
-int TaskSelection()
+int taskSelection()
 {
 	int userChoice;
 	do
 	{
 		scanf_s("%d", &userChoice);
-		while ((getchar()) != '\n');
+		while ((getchar()) != '\n'); //Отчистка входного потока
 		if (userChoice >= 1 && userChoice <= 3)
 		{
 			switch (userChoice)
@@ -48,4 +51,13 @@ int TaskSelection()
 		}
 		else printf("%s", "Неправильный ввод, повторите попытку\n");
 	} while (!(userChoice >= 1 && userChoice <= 3));
+}
+
+double* solutionQuadraticEquation(double a, double b, double c)
+{
+	double D = pow(b, 2) - 4 * a * c;
+	double* quadraticEquationResult = (double*)malloc(sizeof(double) * 2);
+	quadraticEquationResult[0] = (-b + sqrt(D)) / (2 * a);
+	quadraticEquationResult[1] = (-b - sqrt(D)) / (2 * a);
+	return quadraticEquationResult;
 }
