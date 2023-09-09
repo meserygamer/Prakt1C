658aff042a6ee3d5d8306245c5b1d4d1968bf002
MeseryGamer <meserygamer@outlook.com>
2023-09-09 14:00:33 +0300

    Добавлена функция ввода данных пользователя в функции (не доделана)

--- Prakt1/Source.c
+++ Prakt1/Source.c
@@ -10,6 +10,7 @@ int taskSelection();
 double* solutionQuadraticEquation(double a, double b, double c);
 int solutionNumberOfObjectPermutations(int NumberOfObjects);
 double* solvingSystemOfTwoLinearEquations(double** TwoLinearEquationsMatrix);
+void startCalculate(int userChoice);
 
 #pragma endregion
 
@@ -17,7 +18,9 @@ int main()
 {
 	startInfoToConsole(); //Настройка консоли и вывод стартовой информации
 	int userChoice = taskSelection(); //Выбор пользователем задачи 
-	double** Test;
+	startCalculate(userChoice); //Начало решения задачи
+
+	/*double** Test;
 	Test = (double**)malloc(sizeof(double*) * 2);
 	Test[0] = (double*)malloc(sizeof(double) * 3);
 	Test[0][0] = 1;
@@ -27,10 +30,12 @@ int main()
 	Test[1][0] = 3;
 	Test[1][1] = -2;
 	Test[1][2] = -4;
-	solvingSystemOfTwoLinearEquations(Test);
+	solvingSystemOfTwoLinearEquations(Test);*/
 	return 0;
 }
-
+/// <summary>
+/// Настройка консоли и вывод стартовой информации
+/// </summary>
 void startInfoToConsole()
 {
 	SetConsoleCP(1251);
@@ -38,6 +43,10 @@ void startInfoToConsole()
 	printf("Выберите задачу для решения:\n1) Квадратное уравнение\n2) Cистема двух линейных уравнений\n3) Перестановки (факториал)\n");
 }
 
+/// <summary>
+/// Выбор задачи пользователем
+/// </summary>
+/// <returns>Номер выбранной задачи</returns>
 int taskSelection()
 {
 	int userChoice;
@@ -63,6 +72,74 @@ int taskSelection()
 		}
 		else printf("%s", "Неправильный ввод, повторите попытку\n");
 	} while (!(userChoice >= 1 && userChoice <= 3));
+}
+
+/// <summary>
+/// Начало решения задачи
+/// </summary>
+/// <param name="userChoice">Номер выбранной задачи</param>
+void startCalculate(int userChoice)
+{
+	switch (userChoice)
+	{
+	case 1:
+		printf("Введите коэффициенты квадратного уравнения через пробел (Маска ввода: a b c) \n");
+		double a, b, c;
+		int rightNumberCount;
+		do
+		{
+			rightNumberCount = scanf_s("%lf %lf %lf", a, b, c);
+			if(rightNumberCount != 3)
+			{
+				printf("Одно или несколько значений не соответствуют маске, повторите ввод");
+				while ((getchar()) != '\n');
+			}
+		} while (rightNumberCount != 3);
+		//
+		break;
+	case 2:
+		printf("Введите коэффициенты линейных уравнений через пробел (Маска ввода: a1 b1 c1 a2 b2 c2) \n");
+		double** twoLinearEquationsMatrix =  (double**)malloc(sizeof(double*) * 2);
+		int rightNumberCount;
+		for (int i = 0; i < 2; i++)
+		{
+			twoLinearEquationsMatrix[i] = (double*)malloc(sizeof(double) * 3);
+		}
+		do
+		{
+			rightNumberCount = scanf_s("%lf %lf %lf %lf %lf %lf", twoLinearEquationsMatrix[0][0]
+			,twoLinearEquationsMatrix[0][1], twoLinearEquationsMatrix[0][2], twoLinearEquationsMatrix[1][0]
+			,twoLinearEquationsMatrix[1][1], twoLinearEquationsMatrix[1][2]);
+			if (rightNumberCount != 6)
+			{
+				printf("Одно или несколько значений не соответствуют маске, повторите ввод");
+				while ((getchar()) != '\n');
+			}
+		} while (rightNumberCount != 6);
+		break;
+		//
+	case 3:
+		printf("Введите количество предметов, для расчета количества вариантов перестановок \n");
+		int NumberOfObjects, rightNumberCount;
+		do
+		{
+			rightNumberCount = scanf_s("%d", NumberOfObjects);
+			if (rightNumberCount != 1)
+			{
+				printf("Введенное значение не является числом, повторите ввод");
+				while ((getchar()) != '\n');
+			}
+			else
+			{
+				if (NumberOfObjects < 0)
+				{
+					printf("Введено отрицательное количество объектов, повторите ввод");
+				}
+			}
+		} while (rightNumberCount != 1 || NumberOfObjects < 0);
+		//
+		break;
+	}
 }
 
 double* solutionQuadraticEquation(double a, double b, double c)
